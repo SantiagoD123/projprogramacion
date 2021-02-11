ecf1a7c72f1e4c515cd4ab3d42ab1689ebd8c961
SantiagoD123 <iefaz.santiagodiaz@gmail.com>
2021-02-11 07:42:59 -0500

    clase 2

---
+++ clases/clase2.py
@@ -0,0 +1,19 @@
+#booleans son verdadero falso
+Pruebav = True
+Pruebaf = False
+print (Pruebav)
+print (Pruebaf)
+Pruebav = Pruebaf
+print (Pruebav)
+edad = 18
+estatura = 1.86
+peso = 62
+nombre = "Santiago Díaz"
+# utiliza los numerales para generar un marco en el print
+print ("#"*15, "mayor edad", "#"*15)
+ismayoredad = edad >= 18
+print (ismayoredad)
+print ("#"*15, "bajo la estatura promedio", "#"*15)
+ismayorestura = estatura < 1.70
+print (ismayorestura)
+#diferente de se escribe != 
---
+++ clases/operaciones.py
@@ -0,0 +1,6 @@
+numeroA = 47
+numeroB= 50
+
+#Para el expontete se utiliza doble asterisco
+exponente = numeroA**numeroB
+print(exponente)
