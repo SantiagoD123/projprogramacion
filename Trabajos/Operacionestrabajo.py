1f105fb7f995f7baf797cb055e4b78b79dd8c238
SantiagoD123 <iefaz.santiagodiaz@gmail.com>
2021-02-15 19:31:28 -0500

    Trabajo de clase

---
+++ Trabajos/Operacionestrabajo.py
@@ -0,0 +1,19 @@
+A = 46
+B = 58
+
+
+sumar = A + B
+print ("el resultado de la suma es:", sumar)
+
+restar = A - B
+print (f"la resta dio {restar} exitosamente")
+
+multiplicar = A * B
+print ("el resultado de la multiplicacion es:", multiplicar)
+
+
+dividir = A / B
+print (f"la dividir dio {dividir} exitosamente")
+
+exponente = A**B
+print ("el resultado del exponenciación es:", exponente)
--- Trabajos/Trabajo1.py
+++ Trabajos/Trabajo1.py
@@ -15,3 +15,4 @@ isbmayor = B != A
 print (isbmayor)
 
 
+
